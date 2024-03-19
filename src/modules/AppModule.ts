34b73b3415a7ad92c69a548faa3cc4558143bc3a
Warths <pro.warths@gmail.com>
2024-03-19 21:04:33 +0100

    Added boilerplate code to load all modules

--- src/modules/AppModule.ts
+++ src/modules/AppModule.ts
@@ -2,13 +2,18 @@ import { Module } from "@modular/core";
 import { TwitchChatModule } from "./TwitchChatModule/TwitchChatModule";
 import { ChatCommandModule } from "src/modular/modules/ChatCommandModule/ChatCommandModule";
 import { DatabaseService } from "src/modular/services/databaseService/databaseService";
-import { DataStore } from "src/modular/services/DataStore/DataStore";
+import { TwitchScheduledMessageModule } from "./ScheduledMessageModule/ScheduledMessageModule";
+import { ChatLanguageDetectionModule } from "../modular/modules/ChatLanguageDetection/ChatLanguageDetection";
+import { FrenchDetectionModule } from "./ChatLanguageDetectionModule copy/FrenchDetectionModule";
+import { EnglishDetectionModule } from "./ChatLanguageDetectionModule copy/EnglishDetectionModule";
 
 @Module([
     TwitchChatModule,
     ChatCommandModule,
-    DatabaseService
+    DatabaseService,
+    TwitchScheduledMessageModule,
+    ChatLanguageDetectionModule,
+    FrenchDetectionModule, 
+    EnglishDetectionModule
 ])
-export class AppModule {
-    constructor(store: DataStore) {}
-} 
+export class AppModule {} 
